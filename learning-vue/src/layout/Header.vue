27ba4e79dc33678ba02b22c84e7da358fa989bc0
lazarkulasevic <lazar.kulasevic@gmail.com>
2020-10-18 20:45:36 +0200

    vue chrash course first commit

---
+++ learning-vue/src/layout/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <img src="../assets/logo.png">
+    <h1>To-do List</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+  .header {
+    background: #333;
+    color: white;
+    text-align: center;
+    padding: 10px;
+  }
+
+  .header a {
+    color: white;
+    padding-right: 5px;
+  }
+
+  .header img {
+    width: 60px;
+  }
+</style>
